3f2db9c199233183e341dce5bab5e36e5edcab1c
HectorMRC <thehector1593@gmail.com>
2021-12-12 18:40:31 +0100

    updates

--- day_3/main.go
+++ day_3/main.go
@@ -24,7 +24,7 @@ func gammaRate(s *bufio.Scanner, base []int) {
 	for i, b := range s.Text() {
 		if b == '1' {
 			base[i] += 1
-		} else if b == '0' {
+		} else {
 			base[i] -= 1
 		}
 	}
@@ -51,6 +51,97 @@ func GammaRate(r io.Reader) (int64, error) {
 	return strconv.ParseInt(str, 2, BufSize+1)
 }
 
+func oxygenGeneratorRating(bytes []string, index int) string {
+	if l := len(bytes); l == 1 {
+		return bytes[0]
+	} else if l < 1 {
+		return ""
+	}
+
+	var zeros []string
+	var ones []string
+
+	for _, item := range bytes {
+		if item[index] == '0' {
+			zeros = append(zeros, item)
+		} else {
+			ones = append(ones, item)
+		}
+	}
+
+	if len(ones) >= len(zeros) {
+		return oxygenGeneratorRating(ones, index+1)
+	} else {
+		return oxygenGeneratorRating(zeros, index+1)
+	}
+}
+
+func co2ScrubberRating(bytes []string, index int) string {
+	if l := len(bytes); l == 1 {
+		return bytes[0]
+	} else if l < 1 {
+		return ""
+	}
+
+	var zeros []string
+	var ones []string
+
+	for _, item := range bytes {
+		if item[index] == '0' {
+			zeros = append(zeros, item)
+		} else {
+			ones = append(ones, item)
+		}
+	}
+
+	if len(zeros) <= len(ones) {
+		return co2ScrubberRating(zeros, index+1)
+	} else {
+		return co2ScrubberRating(ones, index+1)
+	}
+}
+
+func LifeSupoprtRating(r io.Reader) (int64, int64, error) {
+	var zeros []string
+	var ones []string
+
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanLines)
+	for s.Scan() {
+		if item := s.Text(); item[0] == '0' {
+			zeros = append(zeros, item)
+		} else {
+			ones = append(ones, item)
+		}
+	}
+
+	var oxygen string
+	if len(ones) >= len(zeros) {
+		oxygen = oxygenGeneratorRating(ones, 1)
+	} else {
+		oxygen = oxygenGeneratorRating(zeros, 1)
+	}
+
+	var co2 string
+	if len(zeros) <= len(ones) {
+		co2 = co2ScrubberRating(zeros, 1)
+	} else {
+		co2 = co2ScrubberRating(ones, 1)
+	}
+
+	gen, err := strconv.ParseInt(oxygen, 2, BufSize+1)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	scr, err := strconv.ParseInt(co2, 2, BufSize+1)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return gen, scr, nil
+}
+
 func main() {
 	f, err := os.Open(InputPath)
 	if err != nil {
@@ -69,4 +160,11 @@ func main() {
 
 	epsilon := gamma ^ BitMask
 	fmt.Printf("%v (gamma rate) x %v (epsilon rate): %v\n", gamma, epsilon, gamma*epsilon)
+
+	oxygen, co2, err := LifeSupoprtRating(&buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%v (oxygen generator rate) x %v (CO2 scrubber rate): %v\n", oxygen, co2, oxygen*co2)
 }
